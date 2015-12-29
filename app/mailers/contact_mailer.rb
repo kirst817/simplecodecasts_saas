bf2761ded245b9e4fdc3934dea3bb79973bece36
Kirsten Wedde <kirstenwedde@gmail.com>
2015-12-29 21:34:10 +0000

    Updated more

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,11 @@
 class ContactMailer < ActionMailer::Base
+  default to: 'kirstenwedde@gmail.com'
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
     
-    default to: 'Kirstenwedde@gmail.com'
-   
-   def contact_email(name, email, body)
-       @name = name
-       @email = email
-       @body = body
-       
-       mail(from: email, subject: 'Contact Form Message')
-   end
+    mail(from: email, subject: 'Contact Form Message')
+  end
 end
